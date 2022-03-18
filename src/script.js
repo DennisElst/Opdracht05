a43fa59f6fd50e0bb2017216ff7e47e92d9c0a90
Elst <denniselst1@gmail.com>
2022-03-18 18:54:37 +0100

    toevoegen ogen, veranderen kleur

--- src/script.js
+++ src/script.js
@@ -1,11 +1,22 @@
 let e1, e2, e3;
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(1425, 750);
   noStroke();
-  e1 = new Eye(250, 16, 120);
-  e2 = new Eye(164, 185, 80);
-  e3 = new Eye(420, 230, 220);
+  /* Horizontaal, verticaal, grootte */
+  e1 = new Eye(65, 100, 120);
+  e2 = new Eye(100, 220, 80);
+  e3 = new Eye(250, 150, 220);
+  e4 = new Eye(300, 300, 64);
+  e5 = new Eye(470, 100, 176);
+  e6 = new Eye(564, 200, 40);
+  e7 = new Eye(620, 210, 62);
+  e8 = new Eye(700, 100, 188);
+  e9 = new Eye(850, 100, 97);
+  e10 = new Eye(1000, 100, 175);
+  e11 = new Eye(1220, 100, 23);
+  e12 = new Eye(1400, 100, 45);
+
 }
 
 function draw() {
@@ -13,9 +24,29 @@ function draw() {
   e1.update(mouseX, mouseY);
   e2.update(mouseX, mouseY);
   e3.update(mouseX, mouseY);
+  e4.update(mouseX, mouseY);
+  e5.update(mouseX, mouseY);
+  e6.update(mouseX, mouseY);
+  e7.update(mouseX, mouseY);
+  e8.update(mouseX, mouseY);
+  e9.update(mouseX, mouseY);
+  e10.update(mouseX, mouseY);
+  e11.update(mouseX, mouseY);
+  e12.update(mouseX, mouseY);
+
   e1.display();
   e2.display();
   e3.display();
+  e4.display();
+  e5.display();
+  e6.display();
+  e7.display();
+  e8.display();
+  e9.display();
+  e10.display();
+  e11.display();
+  e12.display();
+
 }
 
 function Eye(tx, ty, ts) {
@@ -34,7 +65,7 @@ function Eye(tx, ty, ts) {
     fill(255);
     ellipse(0, 0, this.size, this.size);
     rotate(this.angle);
-    fill(153, 204, 0);
+    fill(46, 46, 46);
     ellipse(this.size / 4, 0, this.size / 2, this.size / 2);
     pop();
   };
